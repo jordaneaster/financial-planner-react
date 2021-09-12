dd3038390528a713e33f7581aacd918e7305f7ba
jordaneaster <jordaneaster222@icloud.com>
2021-09-12 15:44:23 -0400

    react-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 174.31,
+      date: new Date(2021, 8, 26),
+    },
+    {
+      id: "e2",
+      title: "Phone Bill",
+      amount: 90.0,
+      date: new Date(2021, 8, 14),
+    },
+    {
+      id: "e3",
+      title: "Car Note",
+      amount: 402.12,
+      date: new Date(2021, 9, 1),
+    },
+    {
+      id: "e4",
+      title: "Loan",
+      amount: 280.0,
+      date: new Date(2021, 9, 1),
+    },
+    {
+      id: "e5",
+      title: "Fines",
+      amount: 75.32,
+      date: new Date(2021, 9, 1),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Financial Planner- A React Web Application created by Jordan Easter</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
